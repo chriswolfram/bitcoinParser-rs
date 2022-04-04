1ff911179694258557d17b4f6a98f611b638e5a5
Christopher Wolfram <chriscwolfram@gmail.com>
2022-04-03 22:40:32 -0500

    Reorganized.
    Added exchange rates.

---
+++ src/exchange_rates/mod.rs
@@ -0,0 +1,24 @@
+use chrono::prelude::*;
+use std::collections::HashMap;
+
+use crate::bitcoin_parser::{BitcoinTransaction};
+
+mod rates_rable;
+
+pub struct ExchangeRates {
+	rates_table: HashMap<chrono::Date<Utc>, f64>
+}
+
+impl ExchangeRates {
+	pub fn new() -> ExchangeRates {
+		ExchangeRates {
+			rates_table: rates_rable::get_rates_table()
+		}
+	}
+}
+
+impl BitcoinTransaction {
+	pub fn value_usd(self: &BitcoinTransaction, rates: &ExchangeRates) -> Option<f64> {
+		rates.rates_table.get(&self.timestamp.date()).map(|r| r * self.value() as f64)
+	}
+}
